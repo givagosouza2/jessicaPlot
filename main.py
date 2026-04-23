4c755f401afafec3ab2058c7ea2f491381f66a04
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2026-04-23 17:27:08 -0300

    Add video and set layout to wide in Streamlit app

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+st.set_page_config(layout="wide")
+
 st.title("Vídeo + Cinemática específica")
 
 video_url = "https://youtu.be/KrsJxEfyyVM"
