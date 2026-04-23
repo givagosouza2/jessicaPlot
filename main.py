da9702e6c037aad1ccfbdc2e5593c94a28d6cc7b
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2026-04-23 17:47:46 -0300

    Remove Z-axis plotting and peak detection logic
    Removed code for plotting Z-axis data and peak detection.

--- main.py
+++ main.py
@@ -73,21 +73,4 @@ with col2:
 
 
 
-df.columns = ["tempo", "x", "y", "z"]
-z = df["z"]
-t = df["tempo"]
-peak_z = np.max(z)
-for index,valor in enumerate(z):
-    if valor == peak_z:
-        tempo = t - t[index]
-        break
-# Plot apenas do eixo Z
-fig = px.line(
-    df,
-    x=tempo,
-    y=z,
-    labels={"tempo": "Tempo (s)", "z": "Amplitude"},
-    title="Cinemática - eixo Z"
-)
 
-st.plotly_chart(fig, use_container_width=True)
