501a6e0479aa9cf3abb1cf89618259afab9668a3
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2026-04-23 17:34:43 -0300

    Replace pd.max with np.max for peak_z calculation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import numpy as np
 
 st.set_page_config(layout="wide")
 
@@ -15,7 +16,7 @@ df = pd.read_csv("cinematica.csv", usecols=[0, 9, 10, 11])
 df.columns = ["tempo", "x", "y", "z"]
 z = df["z"]
 t = df["tempo"]
-peak_z = pd.max(z)
+peak_z = np.max(z)
 for index,valor in z:
     if valor == peak_z:
         tempo = t - t[index]
